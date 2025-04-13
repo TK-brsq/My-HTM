29ca0a6e6b29a655b99e7eda0cd93265b85eb927
TKph <tk.0311.h@gmail.com>
2025-04-14 00:19:53 +0900

    20250414

--- HTM.py
+++ HTM.py
@@ -1,78 +1,91 @@
-from dataclasses import dataclass
-from typing import List, Dict
+from dataclasses import dataclass, field
+from typing import List, Dict, Tuple
 import numpy as np
 import random
 
 @dataclass
+class Synapse:
+    target: Tuple[int, int, int]
+    weight: float
+
+@dataclass
 class Segment:
     seg_id: int
-    synapses: List[Dict]
+    num_synapses: int
+    synapses: List[Synapse] = field(default_factory=list)
 
-    def add_synapse(self, source_idx: int, weight: float):
-        self.synapses.append({'source_idx': source_idx, 'weight': weight})
+    def add_synapse(self, target: Tuple[int, int, int], weight: float):
+        if len(self.synapses) >= self.num_synapses:
+            raise ValueError('maximum synapse limit in segment')
+        self.synapses.append(Synapse(target, weight))
     
-    def get_active_synapses(self, threshold=0.2):
-        return [s for s in self.synapses if s['weight'] >= threshold]
+    def get_active_source(self, w_threshold=0.2):
+        active_sy = (self.synapses_weight >= w_threshold)
+        return self.synapses_src[active_sy]
 
 @dataclass
 class Cell:
     cel_id: int
-    segments: List[Segment]
+    num_segments: int
+    segments: List[Segment] = field(default_factory=list)
+    num_synapses: int = 64
 
-    def add_segment(self, seg_id=0, synapses=[]):
-        self.segments.append(Segment(seg_id, synapses))
+    def __post_init__(self):
+        for seg_id in range(self.num_segments):
+            self.segments.append(Segment(seg_id, self.num_synapses))
 
 @dataclass
 class Column:
     col_id: int
-    synapses: List[Dict]
-    cells: List[Cell]
+    num_cells: int
+    num_synapses: int
+    cells: List[Cell] = field(default_factory=list)
+    synapses_src: np.ndarray = field(init=False)
+    synapses_weights: np.ndarray = field(init=False)
 
     def __post_init__(self):
-        for cell in self.cells:
-            cell.add_segment()
+        for cell_id in range(self.num_cells):
+            self.cells.append(Cell(cell_id, 32))
+        self.synapses_src = np.random.choice(1024, self.num_synapses, replace=False)
+        self.synapses_weights = np.random.normal(0.2, 0.025, self.num_synapses)
+
     def get_active_synapses_idx(self, w_threshold=0.2):
-        return [idx for idx, weight in self.synapses.items() if weight >= w_threshold]
+        active_sy = (self.synapses_weight >= w_threshold)
+        return self.synapse_sources[active_sy]
+
 
 @dataclass
 class SpatialPooler:
     input_size: int
     num_columns: int
-    synapse_per_col: int
-    cells_per_col: int
-    columns: List[Column]
+    num_synapses: int
+    num_cells: int
+    columns: List[Column] = field(default_factory=list)
 
     def __post_init__(self):
         for col_id in range(self.num_columns):
-            cells = [Cell(cel_id, []) for cel_id in range(self.cells_per_col)]
-
-            source_idx = np.random.choice(self.input_size, self.synapse_per_col, replace=False)
-            synapses = [{'source_idx': idx, 'weight': random.gauss(0.2, 0.03)}\
-                        for idx in source_idx]
-            column = Column(col_id, synapses, cells)
+            column = Column(col_id, self.num_cells, self.num_synapses)
             self.columns.append(column)
 
     def calc_overlap(self, input: np.ndarray, o_threshold: int) -> np.ndarray:
         overlaps = np.zeros(self.num_columns, dtype=int)
         for col_id, column in enumerate(self.columns):
             active_synapses_idx = column.get_active_synapses_idx()
-            overlap = sum(1 for idx in active_synapses_idx if input[idx] > 0)
-            if overlap < o_threshold:
-                overlap = 0
-            overlaps[col_id] = overlap
+            overlap = np.sum(input[active_synapses_idx] > 0)
+            overlaps[col_id] = overlap if overlap >= o_threshold else 0
         return overlaps
     
     def inhibitation(self, overlaps: np.ndarray, num_active_col: int) -> List[int]:
         if num_active_col > len(overlaps):
             return list(range(len(overlaps)))
-        active_idx = np.argpartition(-overlaps, num_active_col)[num_active_col]
-        return active_idx.tolist()
+        top_k_idx = np.argsort(overlaps)[-num_active_col:]
+        return top_k_idx.tolist()
     
     def learn(self, active_col_idx, w_threshold: float):
         for idx in active_col_idx:
             column = self.columns[idx]
-            for s in column.synapses:
-                if s['weight'] >= w_threshold:
-                    s['weight'] = min(1, s['weight'] + 0.03)
-                else:
-                    s['weight'] = max(0, s['weight'] - 0.02)
+            weights = column.synapses_weights
+            up_idx = weights >= w_threshold
+            down_idx = ~up_idx
+            weights[up_idx] = np.clip(weights[up_idx] + 0.02, 0.0, 1.0)
+            weights[down_idx] = np.clip(weights[down_idx] - 0.02, 0.0, 1.0)
